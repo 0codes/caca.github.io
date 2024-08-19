b9e483521289de1147db2b7198caeded586289aa
Zero <109362284+0codes@users.noreply.github.com>
2024-08-19 18:02:02 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,8 @@
 window.addEventListener('load', function() {
-  // Verifica que todas las hojas de estilo estén cargadas
-  let allStylesLoaded = Array.from(document.styleSheets).every(styleSheet => {
-    return styleSheet.cssRules || styleSheet.rules;
-  });
-
-  if (allStylesLoaded) {
-    // Oculta la pantalla de carga y desplaza la ventana al inicio
-    document.querySelector('.loading-screen').style.display = 'none';
-    window.scrollTo(0, 0);
-  } else {
-    // Si alguna hoja de estilo no está cargada, vuelve a comprobar después de un tiempo
-    setTimeout(function() {
-      window.dispatchEvent(new Event('load'));
-    }, 100);
-  }
+  document.querySelector('.loading-screen').style.display = 'none';
+  window.scrollTo(0, 0);
 });
+
 
 // Selección de elementos
 const openMenuBtn = document.getElementById('open-menu');
